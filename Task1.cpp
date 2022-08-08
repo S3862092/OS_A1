7b8fa96c459f2c5fcb3acc1e425b636fbcad5319
Kai Run Leong <s3862092@student.rmit.edu.au>
2022-08-08 15:52:10 +1000

    Fix sorting now

--- Task1.cpp
+++ Task1.cpp
@@ -1,11 +1,67 @@
-#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
 
-void TaskFilter();
+using std::cout;
+using std::endl;
+
+void TaskFilter(std::string fileName);
+std::string* getWordsFromFile(std::string fileName, int numberOfLines);
+int countLines(std::string fileName);
 
 int main(){
+    std::string fileName = "data/clean_data/fullList.txt";
+    TaskFilter(fileName);
+
+    return EXIT_SUCCESS;
+}
+
+void TaskFilter(std::string fileName){
+    std::ifstream file(fileName);
+    std::ofstream output;
+    int numberOfLines = countLines(fileName);
+    
+    //sort file
+    std::string* wordsArr = getWordsFromFile(fileName, numberOfLines);
+    for(int i = 0; i < 1; i++){
+        cout << wordsArr[i] << endl;
+    }
+    file.close();
+}
+
+std::string* getWordsFromFile(std::string fileName, int numberOfLines){
+    std::ifstream file(fileName);
+    std::string* wordsArr = new std::string[numberOfLines];
+    if(file.is_open()){
+        int index = 0;
+        std::string line;
+        while(getline(file, line)){
+            wordsArr[index] = line;
+            index++;
+        }
+    }
+    return wordsArr;
+}
+
+int countLines(std::string fileName){
+    std::ifstream file(fileName);
+    int lines = 0;
+    std::string line;
+    if(file.is_open()){
+        while(getline(file, line)){
+            lines++;
+        }
+    }else{
+        cout << "ERROR, FILE IS CLOSED!" << endl;
+    }
+    return lines;
+}
+
+void deleteNonPrintable(){
 
 }
 
-void TaskFilter(){
-    
+void deleteThreeConsecutive(){
+
 }
